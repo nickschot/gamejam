005e1865b91e327ef9c1b426a1188edcb7816be3
Nick Schot <nickschot@gmail.com>
2015-12-12 19:39:48 +0000

    Create basic hud, watch all files in game folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
     watch: {
       scripts: {
         files: [
-            'game/**/*.js',
+            'game/**/*.*',
             '!game/main.js'
         ],
         options: {
