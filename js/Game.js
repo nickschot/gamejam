e67c80354cabda43b894a4adc8f9fe75eacc4657
Nick Schot <nickschot@gmail.com>
2015-12-12 14:16:03 +0000

    Load our own map

--- js/Game.js
+++ js/Game.js
@@ -5,28 +5,30 @@ TopDownGame.Game = function(){};
 
 TopDownGame.Game.prototype = {
   create: function() {
-    this.map = this.game.add.tilemap('level1');
+    this.map = this.game.add.tilemap('testmap');
 
     //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
-    this.map.addTilesetImage('tiles', 'gameTiles');
+    this.map.addTilesetImage('tileset', 'mapTiles');
 
     //create layer
-    this.backgroundlayer = this.map.createLayer('backgroundLayer');
-    this.blockedLayer = this.map.createLayer('blockedLayer');
+    this.backgroundlayer = this.map.createLayer('Ground');
+    this.backgroundCornersLayer = this.map.createLayer('GroundCorners');
+    this.backgroundBordersLayer = this.map.createLayer('GroundBorders');
+    //this.blockedLayer = this.map.createLayer('blockedLayer');
 
     //collision on blockedLayer
-    this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+    //this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
 
     //resizes the game world to match the layer dimensions
     this.backgroundlayer.resizeWorld();
 
-    this.createItems();
-    this.createDoors();    
+    //this.createItems();
+    //this.createDoors();    
 
     //create player
-    var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
-    this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
-    this.game.physics.arcade.enable(this.player);
+    //var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
+    //this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
+    //this.game.physics.arcade.enable(this.player);
 
     //the camera will follow the player in the world
     //this.game.camera.follow(this.player);
@@ -35,7 +37,7 @@ TopDownGame.Game.prototype = {
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
   },
-  createItems: function() {
+  /*createItems: function() {
     //create items
     this.items = this.game.add.group();
     this.items.enableBody = true;
@@ -54,9 +56,9 @@ TopDownGame.Game.prototype = {
     result.forEach(function(element){
       this.createFromTiledObject(element, this.doors);
     }, this);
-  },
+  },*/
 
-  //find objects in a Tiled layer that containt a property called "type" equal to a certain value
+  /*//find objects in a Tiled layer that containt a property called "type" equal to a certain value
   findObjectsByType: function(type, map, layer) {
     var result = new Array();
     map.objects[layer].forEach(function(element){
@@ -78,16 +80,16 @@ TopDownGame.Game.prototype = {
       Object.keys(element.properties).forEach(function(key){
         sprite[key] = element.properties[key];
       });
-  },
+  },*/
   update: function() {
     //collision
-    this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    /*this.game.physics.arcade.collide(this.player, this.blockedLayer);
     this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
     this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null, this);
-
+*/
     //player movement
     
-    this.player.body.velocity.x = 0;
+    //this.player.body.velocity.x = 0;
 
     if (this.cursors.up.isDown)
     {
@@ -106,7 +108,7 @@ TopDownGame.Game.prototype = {
     {
         this.game.camera.x += 4;
     }
-  },
+  }/*,
   collect: function(player, collectable) {
     console.log('yummy!');
 
@@ -115,5 +117,5 @@ TopDownGame.Game.prototype = {
   },
   enterDoor: function(player, door) {
     console.log('entering door that will take you to '+door.targetTilemap+' on x:'+door.targetX+' and y:'+door.targetY);
-  },
+  },*/
 };
