87088e5d01581da83960d3aaeaa071c482ff1c75
Martijn Hoogesteger <Tijnoz@gmail.com>
2015-12-12 11:06:46 +0000

    Super basic tileset

--- basic.js
+++ basic.js
@@ -1,21 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event){ initialize(); });
 
 function initialize() {
+    
     var renderer = PIXI.autoDetectRenderer(800, 600);
     document.body.appendChild(renderer.view);
     
     // create the root of the scene graph
     var stage = new PIXI.Container();
     
-    // create a texture from an image path
-    var texture = PIXI.Texture.fromImage('assets/p2.jpeg');
-    
-    /* create a tiling sprite ...
-     * requires a texture, a width and a height
-     * in WebGL the image size should preferably be a power of two
-     */
-    var tilingSprite = new PIXI.extras.TilingSprite(texture, renderer.width, renderer.height);
-    stage.addChild(tilingSprite);
+    createBackground(stage);
     
     var count = 0;
     
@@ -25,15 +18,22 @@ function initialize() {
     
         count += 0.005;
     
-        tilingSprite.tileScale.x = 2 + Math.sin(count);
-        tilingSprite.tileScale.y = 2 + Math.cos(count);
-    
-        tilingSprite.tilePosition.x += 1;
-        tilingSprite.tilePosition.y += 1;
+        // Do animation things
     
         // render the root container
         renderer.render(stage);
     
         requestAnimationFrame(animate);
     }
+    
+    function createBackground(stage) {
+        var texture = PIXI.Texture.fromImage('assets/p2.jpeg');
+        var tilingSprite = new PIXI.extras.TilingSprite(texture, renderer.width, renderer.height);
+        tilingSprite.tileScale.x = 0.25;
+        tilingSprite.tileScale.y = 0.25;
+        
+        stage.addChild(tilingSprite);
+    }
+    
+    
     };
