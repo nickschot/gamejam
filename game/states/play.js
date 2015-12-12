6c83a5d6773fe4921344482eaa489873603d92f9
Nick Schot <nickschot@gmail.com>
2015-12-12 15:24:28 +0000

    Use phaser-generator-official

---
+++ game/states/play.js
@@ -0,0 +1,49 @@
+
+  'use strict';
+  function Play() {}
+  Play.prototype = {
+    create: function() {
+      
+      this.map = this.game.add.tilemap('testmap');
+
+      //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
+      this.map.addTilesetImage('tileset', 'mapTiles');
+  
+      //create layer
+      this.backgroundlayer = this.map.createLayer('Ground');
+      this.backgroundCornersLayer = this.map.createLayer('GroundCorners');
+      this.backgroundBordersLayer = this.map.createLayer('GroundBorders');
+  
+      //resizes the game world to match the layer dimensions
+      this.backgroundlayer.resizeWorld();
+      
+      //Set camera to middle of map
+      this.game.camera.x = this.map.widthInPixels/2;
+      this.game.camera.y = this.map.heightInPixels/2;
+      
+      //Set cameraspeed
+      this.cameraSpeed = 10;
+  
+      //move player with cursor keys
+      this.cursors = this.game.input.keyboard.createCursorKeys();
+    },
+    update: function() {
+      if (this.cursors.up.isDown){
+        this.game.camera.y -= this.cameraSpeed;
+      }
+      else if (this.cursors.down.isDown){
+        this.game.camera.y += this.cameraSpeed;
+      }
+      if (this.cursors.left.isDown){
+        this.game.camera.x -= this.cameraSpeed;
+      }
+      else if (this.cursors.right.isDown){
+        this.game.camera.x += this.cameraSpeed;
+      }
+    },
+    clickListener: function() {
+      this.game.state.start('gameover');
+    }
+  };
+  
+  module.exports = Play;
---
+++ game/states/preload.js
@@ -0,0 +1,36 @@
+
+'use strict';
+function Preload() {
+  this.asset = null;
+  this.ready = false;
+}
+
+Preload.prototype = {
+  preload: function() {
+    this.asset = this.add.sprite(this.width/2,this.height/2, 'preloader');
+    this.asset.anchor.setTo(0.5, 0.5);
+
+    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+    this.load.setPreloadSprite(this.asset);
+    this.load.image('yeoman', 'assets/yeoman-logo.png');
+    
+    
+    
+    this.load.tilemap('testmap', 'assets/testmap.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('mapTiles', 'assets/tileset.png');
+
+  },
+  create: function() {
+    this.asset.cropEnabled = false;
+  },
+  update: function() {
+    if(!!this.ready) {
+      this.game.state.start('menu');
+    }
+  },
+  onLoadComplete: function() {
+    this.ready = true;
+  }
+};
+
+module.exports = Preload;
