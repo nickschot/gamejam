8a87e1c3cd47de551ac54c520a8f9418a864bd58
Nick Schot <nickschot@gmail.com>
2015-12-12 16:35:36 +0000

    Add edgescroll

--- game/states/play.js
+++ game/states/play.js
@@ -13,6 +13,8 @@
       this.backgroundlayer = this.map.createLayer('Ground');
       this.backgroundCornersLayer = this.map.createLayer('GroundCorners');
       this.backgroundBordersLayer = this.map.createLayer('GroundBorders');
+      
+      //this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
   
       //resizes the game world to match the layer dimensions
       this.backgroundlayer.resizeWorld();
@@ -30,19 +32,41 @@
     update: function() {
       if (this.cursors.up.isDown){
         this.game.camera.y -= this.cameraSpeed;
-      }
-      else if (this.cursors.down.isDown){
+      }else if (this.cursors.down.isDown){
         this.game.camera.y += this.cameraSpeed;
       }
       if (this.cursors.left.isDown){
         this.game.camera.x -= this.cameraSpeed;
-      }
-      else if (this.cursors.right.isDown){
+      }else if (this.cursors.right.isDown){
         this.game.camera.x += this.cameraSpeed;
       }
+      
+      this.edgeScroll();
     },
     clickListener: function() {
       this.game.state.start('gameover');
+    },
+    
+    edgeScroll: function(){
+      var deadzone = 50;
+      
+      var width = this.game.width;
+      var height = this.game.height;
+      
+      var x = this.game.input.mousePointer.x;
+      var y = this.game.input.mousePointer.y;
+      
+      if(x < deadzone){
+        this.game.camera.x -= this.cameraSpeed;
+      }else if(x > width - deadzone){
+        this.game.camera.x += this.cameraSpeed;
+      }
+      
+      if(y < deadzone){
+        this.game.camera.y -= this.cameraSpeed;
+      } else if(y > height - deadzone){
+        this.game.camera.y += this.cameraSpeed;
+      }
     }
   };
   
