a51ae96a166cf34afab261e57e949fd03f7232d3
Martijn Hoogesteger <Tijnoz@gmail.com>
2015-12-13 13:32:11 +0000

    New splash screen

--- game/states/menu.js
+++ game/states/menu.js
@@ -7,18 +7,20 @@ Menu.prototype = {
 
   },
   create: function() {
-    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
-    this.sprite = this.game.add.sprite(this.game.width/2, 138, 'yeoman');
-    this.sprite.anchor.setTo(0.5, 0.5);
+    var style = { font: '65px sans-serif', fill: '#ffffff', align: 'center', stroke: '#000000', strokeThickness: 10};
+    this.sprite = this.game.add.sprite(0, 0, 'nuclear');
 
-    this.titleText = this.game.add.text(this.game.width/2, 300, 'Robopocalypse!', style);
+    this.titleText = this.game.add.text(this.game.width/2, 140, 'ROBOPOCALYPSE', style);
     this.titleText.anchor.setTo(0.5, 0.5);
+    
+    var story = "Once a vibrant city, after the nuclear disaster five years ago, there is not much left. Protected from the radiation by shields, some people have survived. \
+Today, one very old robot was found, and a way off this destroyed world lies before you. It is up to you to make use of this robot, gather the necessary resources, and find a way to save everyone. \n Goodluck";
+    
+    this.storyText = this.game.add.text(this.game.width/2, 380, story, { font: '20px sans-serif', fill: '#ffffff', align: 'center', wordWrapWidth: 570, wordWrap: true, stroke: '#000000', strokeThickness: 2});
+    this.storyText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.width/2, 400, 'Click anywhere to play', { font: '20px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText = this.game.add.text(this.game.width/2, 580, 'Click anywhere to play', { font: '20px Arial', fill: '#ffffff', align: 'center'});
     this.instructionsText.anchor.setTo(0.5, 0.5);
-
-    this.sprite.angle = -20;
-    //this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
--- game/states/preload.js
+++ game/states/preload.js
@@ -15,6 +15,7 @@ Preload.prototype = {
     this.load.image('yeoman', 'assets/yeoman-logo.png');
     this.load.spritesheet('robot', 'assets/robot.png', 32, 32);
     this.load.spritesheet('city', 'assets/city.png', 32, 32);
+    this.load.image('nuclear', 'assets/nuclear-01.png');
     
     this.load.tilemap('testmap', 'assets/testmap.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('mapTiles', 'assets/tileset.png');
