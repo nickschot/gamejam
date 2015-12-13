a1235f8100ad62a44738367bab282ee5c017821c
Martijn Hoogesteger <Tijnoz@gmail.com>
2015-12-13 16:22:03 +0000

    Better fog, but needs preload

--- game/prefabs/fog.js
+++ game/prefabs/fog.js
@@ -5,18 +5,16 @@ var Fog = function(game, x, y, frame) {
   this.fogTiles = []
   this.game = game;
   this.updates = 0;
-  game.map.createBlankLayer('fog', game.map.width,game.map.height, 32, 32);
-  game.map.addTilesetImage('fog');
+  this.map = game.add.tilemap();
+  this.map.addTilesetImage('fog');
+  this.fogLayer = this.map.create('fog', this.game.map.width, this.game.map.height, this.game.map.tileWidth, this.game.map.tileHeight);
   for(var x = 0; x < this.game.map.width; x++) {
     this.fogTiles[x] = []
     for(var y = 0; y < this.game.map.height; y++) {
-      //game.map.putTile(0,x,y,'fog');
-      this.fogTiles[x][y] = new Phaser.Sprite(game, x * this.game.map.tileWidth , y * this.game.map.tileHeight,'fog');
-      this.fogTiles[x][y].alpha = 0.9;
-      this.addChild(this.fogTiles[x][y]);
+      var tile = this.map.putTile(0,x,y);
+      tile.alpha = 0.9
     }
   }
-  console.log('FOG',this.fogTiles);
 };
 
 Fog.prototype = Object.create(Phaser.Sprite.prototype);
@@ -24,17 +22,25 @@ Fog.prototype.constructor = Fog;
 
 Fog.prototype.update = function() {
   this.updates++;
-  if(this.updates%10==0) {
+  if(this.updates%4==0) {
     var self = this;
     this.game.robots.forEach(function(robot) {
       var tilex = Math.floor(robot.position.x/self.game.map.tileWidth)
       var tiley = Math.floor(robot.position.y/self.game.map.tileHeight)
       for(var x = -2; x <= 2; x++) {
         for(var y = -2; y<= 2; y++) {
-          if(x==0 && y==0)
-            self.removeChild(self.fogTiles[tilex][tiley]);
-          else 
-            self.fogTiles[tilex+x][tiley+y].alpha -= 0.08/(Math.abs(x)+Math.abs(y));
+          var tile = self.map.getTile(tilex-x,tiley-y);
+          if(tile != null) {
+            if(x==0 && y==0)
+              self.map.removeTile(tilex,tiley);
+            else {
+              tile.alpha -= 0.008/(Math.abs(x)+Math.abs(y));
+              if(tile.alpha <= 0) {
+                self.map.removeTile(tilex-x,tiley-y);
+              }
+              self.fogLayer.dirty=true;
+            }
+          }
         }
       }
       //self.fogTiles[tilex][tiley];
